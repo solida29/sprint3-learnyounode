1efa42ae6f3f0b6c5a14c7ec7cae5426c6328409
Solida <solida.var@gmail.com>
2023-11-09 12:56:33 +0100

    time-server.ts ok

--- src/time-server.ts
+++ src/time-server.ts
@@ -4,7 +4,31 @@ const portNumber = process.argv[2];
 
 const server = net.createServer(function (socket) {
   // logique de gestion de la socket
-  socket.write(data, 'utf8');
-  socket.end(data);
+  socket.write(todayDate());
+  socket.end();
 });
 server.listen(portNumber);
+
+function todayDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  let result = '';
+
+  const format = (integers: number) => {
+    if (integers <= 9) {
+      return '0' + integers;
+    } else {
+      return integers;
+    }
+  };
+
+  const month = format(date.getMonth() + 1);
+  const day = format(date.getDate());
+  const hours = format(date.getHours());
+  const minutes = format(date.getMinutes());
+
+  result = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + '\n';
+  console.log(result);
+  return result;
+}
+todayDate();
