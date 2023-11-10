8add70bc9cd569c42d1ca69c6ae9fcb5f91d5d6e
Solida <solida.var@gmail.com>
2023-11-10 23:58:05 +0100

    http-uppercaserer.ts anotaciones

--- src/http-uppercaserer.ts
+++ src/http-uppercaserer.ts
@@ -5,19 +5,22 @@ let result = '';
 
 const server = http.createServer((request, response) => {
   if (request.method === 'POST') {
+    // Si es una solicitud POST
     request.setEncoding('utf8');
     request.on('data', (chunk: WritableStream) => {
       result += chunk.toString().toUpperCase();
+      // Convierte los chunks del body de la solicitud POST a mayúsculas
     });
     request.on('end', () => {
+      // Envia la respuesta con los datos convertidos a mayúsculas
       response.writeHead(200, { 'content-type': 'text/plain' });
       response.end(result);
     });
   } else {
-    response.on('error', () => {
-      response.writeHead(405, { Allow: 'POST' });
-      response.end('Only POST request available');
-    });
+    // Si no es una solicitud POST,
+    // se responde con un código de estado 405 (Método no permitido)
+    response.writeHead(405, { Allow: 'POST' });
+    response.end('Only POST request available');
   }
 });
 server.listen(portNumber);
