179c7694d9c64fed9a00cd49f0612b3e75c1c2ae
Solida <solida.var@gmail.com>
2023-11-14 17:52:41 +0100

    client TEST renombrado

--- src/clientTestSocket.ts
+++ src/clientTestSocket.ts
@@ -6,24 +6,24 @@ function connectToServer(portNumber: number): void {
   const client = new net.Socket();
 
   // Conectar al servidor
-  client.connect(portNumber, 'localhost', function () {
+  client.connect(portNumber, 'localhost', () => {
     console.log(`Connected with server http://localhost:${portNumber}`);
   });
 
   // Datos recibidos del servidor
-  client.on('data', function (data) {
+  client.on('data', (data) => {
     const responseData = data.toString();
     console.log('Server response:', responseData);
     client.end();
   });
 
   // Cierre del evento conexión
-  client.on('close', function () {
+  client.on('close', () => {
     console.log('Connection closed');
   });
 
   // Errores durante la conexión del cliente
-  client.on('error', function (error) {
+  client.on('error', (error) => {
     console.error('Error on client connection:', error.message);
   });
 }
