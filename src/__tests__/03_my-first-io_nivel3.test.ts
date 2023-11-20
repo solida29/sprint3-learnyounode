d82caf4e95e05d642835b5df991a8e5fef678d82
Solida <solida.var@gmail.com>
2023-11-20 23:30:06 +0100

    test NO VA

--- src/__tests__/03_my-first-io_nivel3.test.ts
+++ src/__tests__/03_my-first-io_nivel3.test.ts
@@ -4,6 +4,6 @@ process.argv[2] = '../ScriptTest.md';
 
 describe('Ex03', () => {
   test('contamos el numero de lineas en el archivo', () => {
-    expect(fileContent.toString() + '\n').toBe(3);
+    expect(fileContent.toString() + '\n').toBe(27);
   });
 });
